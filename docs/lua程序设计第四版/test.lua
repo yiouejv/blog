f373c3a376b2ad3a02a0d5d5d4b25b6ef1098bec
unknown <yiouejv@126.com>
2021-03-14 22:25:08 +0800

    [读书笔记]lua程序设计第四版

--- docs/lua程序设计第四版/test.lua
+++ docs/lua程序设计第四版/test.lua
@@ -1,3 +1,34 @@
 #!/usr/bin/lua
 
-print(2^-3^4)
+function insert(str, pos, s)
+    if not pos then return s end
+    if pos == 0 then return str end
+    local head, tail
+    if pos > 0 then
+        head = string.sub(str, 1, pos - 1)
+        tail = string.sub(str, pos)
+    else
+        head = string.sub(str, 1, pos)
+        tail = string.sub(str, pos+1)
+    end
+    if pos == -1 then tail = '' end
+
+    return head .. s .. tail
+end
+
+
+
+print(insert("hello world", 1, "start: "))
+print(insert("hello world", 2, "start: "))
+print(insert("hello world", 3, "start: "))
+print(insert("hello world", 4, "start: "))
+print(insert("hello world", 5, "start: "))
+print(insert("hello world", 6, "start: "))
+print(insert("hello world", 7, "start: "))
+print(insert("hello world", 8, "start: "))
+print(insert("hello world", 9, "start: "))
+print(insert("hello world", 10, "start: "))
+print(insert("hello world", 11, "start: "))
+print(insert("hello world", 12, "start: "))
+print(insert("hello world", 13, "start: "))
+print(insert("hello world", 14, "start: "))
