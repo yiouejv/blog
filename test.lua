8972174330e2fcb18e70a8bf0a87b45a3f3d0ef7
yiouejv <yiouejv@126.com>
2021-03-20 11:59:59 +0000

    游戏事件驱动机制，select, table.pack

--- test.lua
+++ test.lua
@@ -1,3 +1,25 @@
-require("PRINT")
 
-print(table.unpack({1, 2, 3, 4, 5, a=1}))
+
+
+
+
+
+
+
+
+
+
+
+
+function shuffle(sequence)
+    for i, _ in ipairs(sequence) do
+        local j = math.random(i, #sequence)
+        sequence[i], sequence[j] = sequence[j], sequence[i]
+    end
+end
+
+
+local t = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+shuffle(t)
+require("PRINT")
+PRINT(t)
